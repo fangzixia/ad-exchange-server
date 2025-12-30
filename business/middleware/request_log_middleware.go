8a6be3142b6ad596f4a4888700fef89ec341f169
matrix_studio <yaojie.me@qq.com>
2025-12-31 00:56:35 +0800

    init

--- business/middleware/request_log_middleware.go
+++ business/middleware/request_log_middleware.go
@@ -5,6 +5,9 @@ import "ad-exchange-server/core/model"
 func RequestLogMiddleware() MediaHandlerFunc {
 
 	return func(mediaContent *model.AdMediaContent) bool {
+		defer func() {
+			println("RequestLogMiddleware")
+		}()
 		return true
 	}
 
